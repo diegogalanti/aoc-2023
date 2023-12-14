da7f4cb690cb8018d12955b03bbb47a5f9b9e10a
diegogalanti <dggalanti@hotmail.com>
2023-12-14 19:54:09 +0100

    Part 2 implementation

--- src/Day01.kt
+++ src/Day01.kt
@@ -4,12 +4,37 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val digitsMap = hashMapOf(
+            "one" to 1,
+            "1" to 1,
+            "2" to 2,
+            "two" to 2,
+            "3" to 3,
+            "three" to 3,
+            "4" to 4,
+            "four" to 4,
+            "5" to 5,
+            "five" to 5,
+            "6" to 6,
+            "six" to 6,
+            "7" to 7,
+            "seven" to 7,
+            "8" to 8,
+            "eight" to 8,
+            "9" to 9,
+            "nine" to 9
+        )
+        return input.sumOf { line ->
+            "${digitsMap[line.findAnyOf(digitsMap.keys)?.second]}${digitsMap[line.findLastAnyOf(digitsMap.keys)?.second]}".toInt()
+        }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput1 = readInput("Day01_test")
     check(part1(testInput1) == 142)
+
+    val testInput2 = readInput("Day01_test2")
+    check(part2(testInput2) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
