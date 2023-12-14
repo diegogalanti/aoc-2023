dc6ba7124c272573ef865a55823b6cf4682f688e
diegogalanti <dggalanti@hotmail.com>
2023-12-14 19:53:27 +0100

    Part 1 implementation

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,6 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>) = input.sumOf { line ->
+        "${line.first { it.isDigit() }}${line.last { it.isDigit() }}".toInt()
     }
 
     fun part2(input: List<String>): Int {
@@ -8,8 +8,8 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput1 = readInput("Day01_test")
+    check(part1(testInput1) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
