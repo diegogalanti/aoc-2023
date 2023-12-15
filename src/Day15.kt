a704a8d31f585915b007fc5c0d200dc6ba0e9c24
diegogalanti <dggalanti@hotmail.com>
2023-12-15 10:08:03 +0100

    Part 2 implementation

--- src/Day15.kt
+++ src/Day15.kt
@@ -6,14 +6,39 @@ fun main() {
     }
 
     fun part2(input: String): Int {
-        return 0
+        val map = HashMap<String, Pair<Int, Int>>()
+        input.split(',').forEachIndexed { i, token ->
+            val indexOpe = token.indexOfAny(listOf("=", "-"))
+            val key = token.substring(0..<indexOpe)
+            val operation = token[indexOpe]
+            val value = token.substring(indexOpe + 1)
+            if (operation == '=') {
+                if (map[key] == null)
+                    map[key] = Pair(value.toInt(), i)
+                else
+                    map[key] = Pair(value.toInt(), map[key]!!.second)
+            } else
+                map.remove(key)
+        }
+        val boxes = Array(256) { 0 }
+        var result = 0
+        map.toSortedMap(compareBy {
+            map[it]?.second
+        }).forEach {
+            val hash = part1(it.key)
+            boxes[hash]++
+            result += (hash + 1) * boxes[hash] * it.value.first
+        }
+        return result
     }
 
 
 // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day15_test")[0]
     check(part1(testInput) == 1320)
+    check(part2(testInput) == 145)
 
     val input = readInput("Day15")[0]
     part1(input).println()
+    part2(input).println()
 }
